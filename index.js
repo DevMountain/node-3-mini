50355efdfd9fa3836b801cb29789cb8147543c20
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-09-06 17:41:06 -0700

    adjustments to readme and index.js reflecting changes to heroku, sqltabs and .env

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = module.exports = express();
+const app = express();
 app.use( bodyParser.json() );
 app.use( cors() );
 
-const port = 3000;
+const port = process.env.PORT || 3000
 app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
 
