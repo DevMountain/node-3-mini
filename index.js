397fe5989b35704a0cf2f196864a9f2a6d2e532f
mjburtenshaw <31086089+mjburtenshaw@users.noreply.github.com>
2017-09-21 12:50:46 -0600

    complete step 1

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config();
 
 const app = express();
 app.use( bodyParser.json() );
