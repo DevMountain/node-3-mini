abc3f1fcd2e85ff479406662e6a09668c61546e7
Bryan Smith <bryan.gsmith33@gmail.com>
2018-01-17 18:18:38 -0700

    navbar completed

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require('massive');
+require('dotenv').config();
+const plane_controller = require('./controller');
 
 const app = express();
-app.use( bodyParser.json() );
-app.use( cors() );
+app.use(bodyParser.json());
+app.use(cors());
+
+massive(process.env.CONNECTION_STRING)
+  .then(dbInstance => {
+    app.set('db', dbInstance)
+  });
+
+app.get('/api/planes', plane_controller.getPlanes);
 
 const port = process.env.PORT || 3000
-app.listen( port , () => { console.log(`Server listening on port ${port}`); } );
+app.listen(port, () => { console.log(`Server listening on port ${port}`); });
 
